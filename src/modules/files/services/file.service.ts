4e1314c3ec3d0a7c118859ebf3a4dae50c960aa5
rodionros <r.rostovshchikov@yandex.ru>
2025-01-23 13:49:19 +0300

    feat: add logs

--- src/modules/files/services/file.service.ts
+++ src/modules/files/services/file.service.ts
@@ -12,7 +12,7 @@ import {
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { ConfigService } from '@nestjs/config';
-import { firstValueFrom, retry, timeout } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { ClientProxy } from '@nestjs/microservices';
 import { plainToInstance } from 'class-transformer';
 
