6f67a80a5f41d1fccae04f3c79c18ffa91205629
rodionros <r.rostovshchikov@yandex.ru>
2025-01-24 13:46:25 +0300

    feat: queue fix

--- src/modules/files/services/file.service.ts
+++ src/modules/files/services/file.service.ts
@@ -12,7 +12,7 @@ import {
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { ConfigService } from '@nestjs/config';
-import { firstValueFrom, retry } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { ClientProxy } from '@nestjs/microservices';
 import { plainToInstance } from 'class-transformer';
 
@@ -60,9 +60,7 @@ export class FilesService implements IFileService {
       },
     });
     const userResponse = await firstValueFrom(
-      this.authClient
-        .send('getUserById', JSON.stringify({ userId }))
-        .pipe(retry(3)),
+      this.authClient.send('getUserById', JSON.stringify({ userId })),
     );
     const user = plainToInstance(UserResponseDto, userResponse);
     return { ...file, author: user };
